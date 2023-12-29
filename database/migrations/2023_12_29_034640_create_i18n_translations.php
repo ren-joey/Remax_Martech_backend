340e8de5b33394b174c97efc6efe4a4b741084b7
ren-joey <tsuyiren@gmail.com>
2023-12-29 12:21:30 +0800

    feat: 🎸 add db table migrations

---
+++ database/migrations/2023_12_29_034640_create_i18n_translations.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('i18n_translations', function (Blueprint $table) {
+            $table->id();
+            $table->string('i18n_key');
+            $table->foreign('i18n_key')->references('key')->on('i18n')->onDelete('cascade');
+            $table->string('locale', 18);
+            $table->string('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('i18n_translations');
+    }
+};
